d156adbd9cbd25b5fb0e6b14ee2b662364c1892b
Tmrvarun <er.varuntomar@gmail.com>
2026-06-14 08:15:31 +0530

    Update 6. dropdown.spec.ts

--- 6. dropdown.spec.ts
+++ 6. dropdown.spec.ts
@@ -38,3 +38,52 @@ test('dropdown', async()=>{
 
 
 })
+
+-------------------------------------------------------------------
+
+
+import {test,Browser,Locator,Page,Expect} from '@playwright/test'
+import {firefox,chrome,webkit} from 'playwright'
+
+test('drop down', async()=> {
+    const browser:Browser=await firefox.launch({'headless':false})
+    const page:Page=await browser.newPage()
+    await page.goto('https://www.magupdate.co.uk/magazine-subscription/PALI')
+    const dropdown:Locator= await page.locator('select#Contact_CountryCode')    
+
+    // selecting drop down element by value:
+    await dropdown.selectOption({value:'CV'})
+
+    //selecting drop down element by index:
+
+    await dropdown.selectOption({index: 10})
+
+    //selecting drop down element by text/name/label:
+
+    await dropdown.selectOption({label:'India'})
+
+    const elements_select_list=await page.$$('select#Contact_CountryCode > option') // element stores options from element list , $$ is used to store element from the page
+    console.log("Total elements in list is: ",elements_select_list.length)
+    
+ // Loop to iterate elements of the list :
+ 
+    for (const e of elements_select_list)
+    {
+        
+        //storing elements from the loop to element_name 
+        const element_name= await e.textContent()
+        console.log(element_name)
+
+        //stopping loop at the element India and break the loop if element is searched
+
+        if (element_name=='India')
+        {
+            await dropdown.selectOption({label:'India'})
+            break;
+        }
+    }
+        
+
+ await page.waitForTimeout(5000)
+
+})
